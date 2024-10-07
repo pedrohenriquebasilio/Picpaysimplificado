cdd42dfb6f3b60adde2ce53e186764e7d1984f2a
Pedro Henrique Basilio <phbasilio6903@gmail.com>
2024-10-07 09:56:18 -0300

    feat: Initial config

---
+++ src/main/java/com/example/Picpaybackend/PicpaybackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Picpaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Picpaybackend/PicpaybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Picpaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
